3a04635cae1573c85723773a765a4d5ff32ea488
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-05 01:24:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_push(array, string)
   # colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  string = colors_in_the_rainbow.push("violet")
+  string = array.push("violet")
 end
 
 def using_unshift(array, string)
