23c1809f9adb7043d58defb7e00c7bc49bbb0389
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-05 01:03:16 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  string = colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  string = "Staten Island"
+  bouroughs_in_nyc.unshift(string)
+end
+
+def using_pop(array)
+  array = [ "Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica" ]
+  array.pop
+end
+
+# def using_pop(array)
+#   continents = [ "Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica" ]
+#   continents.size
+# end
+
+# def pop_with_args(array)
+#   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+#   dog_breeds.pop(2)
+# end
