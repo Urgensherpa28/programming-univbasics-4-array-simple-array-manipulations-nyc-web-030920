33323ebf5fb6bedffb4d9a8234d24503b33f9f05
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-05 01:24:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,9 @@
 def using_push(array, string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  # colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   string = colors_in_the_rainbow.push("violet")
 end
 
 def using_unshift(array, string)
-  # bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  # string = "Staten Island"
   array.unshift(string)
 end
 
